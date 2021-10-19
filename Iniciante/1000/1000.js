63e72d845443f777586bfb2b88a1afe567fe55d6
Oevidente <andreluiz1902@gmail.com>
2021-10-19 18:46:11 -0300

    First commit

---
+++ Iniciante/1000/1000.js
@@ -0,0 +1,13 @@
+// Função que escreve no console do vs code ou chrome
+console.log("Qualquer coisa");
+
+// As funções abaixo só funcionam no navegador
+//Função que permite um alerta na tela do navegador
+alert('');
+
+//Função escreve no documento html
+document.write('');
+
+//Função para capturar dados do usuário
+let pergunta = prompt("Qual foi?");
+console.log(pergunta);
