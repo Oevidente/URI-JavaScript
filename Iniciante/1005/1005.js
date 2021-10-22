07698e893f5f6495590daa705986ad3a90d743c3
Oevidente <andreluiz1902@gmail.com>
2021-10-22 14:17:28 -0300

    Commit 1005

---
+++ Iniciante/1005/1005.js
@@ -0,0 +1,14 @@
+/* Leia dois valores reais de duas casas ex: 2.0
+Sabendo que pesoNota1 = 3.5 e pesoNota2 = 7.5
+Calcule a média ponderada (nota1 + nota2 / pesoNota1 + pesoNota2)
+OBS: Nota máxima até 10.0
+Imprima "MEDIA = " juntamente com o resultado da expressão */
+
+let nota1 = parseFloat(prompt('Digite a primeira nota'));
+let nota2 = parseFloat(prompt('Digite a segunda nota'));
+let pesoNota1 = 3.5;
+let pesoNota2 = 7.5;
+
+let media = ((nota1*pesoNota1)+(nota2*pesoNota2))/(pesoNota1+pesoNota2);
+
+console.log('MEDIA =', media);
