3d3eb39f3dc22db6de249908ae41248f202b127d
Oevidente <andreluiz1902@gmail.com>
2021-10-22 15:06:52 -0300

    Commit 1008 corrigido

--- Iniciante/1008/1008.js
+++ Iniciante/1008/1008.js
@@ -4,14 +4,13 @@ o valor por hora (real)
 calcule o salário
 Imprima o número do funcionário
 e resultado em numero REAL com R$ */
-
 let funcionario = parseInt(prompt('Digite o número do funcionário:'));
 let horasTrabalhadas = parseInt(prompt('Digite a quantidade de horas trabalhadas:'));
 let valorHora = parseFloat(prompt('Digite o salário-hora:'));
 
-let salário = valorHora * horasTrabalhadas;
+let salário = parseFloat(valorHora * horasTrabalhadas);
 
 alert(`FUNCIONÁRIO = ${funcionario}
-SALÁRIO = ${salário}`);
+SALÁRIO = R$ ${salário}`);
 console.log(`FUNCIONÁRIO = ${funcionario}
-SALÁRIO = ${salário}`);
+SALÁRIO = R$ ${salário}`);
