173540bee8d2b245e9cb9434fddd45e9d36a82a8
Oevidente <andreluiz1902@gmail.com>
2021-10-28 22:12:08 -0300

    1005 atualizado

--- Iniciante/1005/1005.js
+++ Iniciante/1005/1005.js
@@ -4,12 +4,12 @@ Calcule a média ponderada (nota1 + nota2 / pesoNota1 + pesoNota2)
 OBS: Nota máxima até 10.0
 Imprima "MEDIA = " juntamente com o resultado da expressão */
 
-let nota1 = parseFloat(prompt('Digite a primeira nota'));
-let nota2 = parseFloat(prompt('Digite a segunda nota'));
-let pesoNota1 = 3.5;
-let pesoNota2 = 7.5;
+let nota1 = parseFloat(prompt('Digite a primeira nota')).toFixed(1);
+let nota2 = parseFloat(prompt('Digite a segunda nota')).toFixed(1);
+const pesoNota1 = 3.5;
+const pesoNota2 = 7.5;
 
-let media = ((nota1*pesoNota1)+(nota2*pesoNota2))/(pesoNota1+pesoNota2);
+let media = ((nota1 * pesoNota1) + (nota2 * pesoNota2)) / (pesoNota1 + pesoNota2);
 
 console.log('MEDIA =', media.toFixed(5));
 alert(`MEDIA = ${media.toFixed(5)}`);
